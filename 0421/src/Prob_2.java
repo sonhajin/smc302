5ef40f84ca16f6c20c3c10cbf41dc96b9a10b358
hajin <sonhajin1234@gmail.com>
2021-04-22 15:37:32 +0900

    0421 project 완성

---
+++ 0421/src/Prob_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Prob_2 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("1000 이하의 자연수를 입력하세요.");
+		int num1 = scan.nextInt();
+		int i = 0;
+		int sum = 0;
+		
+		while(i<=20) {
+			if(i%4==0) {
+				sum = num1 + i;
+				System.out.println(i);
+			}
+			i++;
+		}
+		sum = num1 + sum;
+		System.out.println("1~"+num1+"까지 4의 배수의 합은 "+sum+"입니다.");
+	}
+}
+   
